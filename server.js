e3ce81c80b84ef99ae49210f0be6932493cab403
Joshua M Cubas <joshuacubas@yahoo.com>
2020-04-07 15:32:16 -0500

    added auth controller stuff

--- server.js
+++ server.js
@@ -3,8 +3,23 @@ const express = require(`express`)
 const server = express()
 const PORT = process.env.PORT
 require(`./db/db.js`)
+const bodyParser = require(`body-parser`)
+const methodOverride = require('method-override')
+const session = require(`express-session`)
+
 
 server.use(express.static(`public`))
+server.use(bodyParser.urlencoded({extended:false}))
+server.use(methodOverride('_method'))
+
+server.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+
+const authController = require(`./controllers/authController.js`)
+server.use(`/auth`, authController)
 
 server.get(`/`, (req,res) => {
 	res.render(`home.ejs`)
