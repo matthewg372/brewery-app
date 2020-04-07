5eef1b8fc5c348f6f5a1aaaf4f6b1a49f511efe8
Joshua M Cubas <joshuacubas@yahoo.com>
2020-04-07 10:18:44 -0500

    serverm runnning on PORT

---
+++ server.js
@@ -0,0 +1,17 @@
+require(`dotenv`).config()
+const express = require(`express`)
+const server = express()
+const PORT = process.env.PORT
+
+
+
+
+
+
+
+
+
+
+server.listen(PORT, () => {
+	console.log(`Server running on port, ${PORT}`)
+})
