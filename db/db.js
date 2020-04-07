eea29078702a98d2889f9e06593aa2b53e5061e2
Joshua M Cubas <joshuacubas@yahoo.com>
2020-04-07 10:55:33 -0500

    added db/db.js

--- db/db.js
+++ db/db.js
@@ -1,1 +1,21 @@
 const mongoose = require(`mongoose`)
+
+const connectionString = process.env.MONGODB_URI
+
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to database ${connectionString}`);
+})
+mongoose.connection.on('disconnected', () => {
+  console.log(`Disonnected from database ${connectionString}`);
+})
+mongoose.connection.on('error', (err) => {
+  console.log(`Error connecting to ${connectionString}:`)
+  console.log(err);
+  console.dir(err);
+})
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require(`dotenv`).config()
 const express = require(`express`)
 const server = express()
 const PORT = process.env.PORT
+require(`./db/db.js`)
 
 server.use(express.static(`public`))
 
