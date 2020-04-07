cbedbcb59592a16c02d38f13990bd2f5b8ad7b23
Joshua M Cubas <joshuacubas@yahoo.com>
2020-04-07 10:49:50 -0500

    setup partials, homepage, css

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require(`express`)
 const server = express()
 const PORT = process.env.PORT
 
+server.use(express.static(`public`))
 
-
-
+server.get(`/`, (req,res) => {
+	res.render(`home.ejs`)
+})
 
 
 
